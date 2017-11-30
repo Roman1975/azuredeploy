9ca4d8848bb50a0c099fa85e6a2f0e5545c6794d
Roman <roman@Romans-MacBook-Pro.local>
2017-11-30 16:49:46 +0200

    logging for azure deploy
    this commit will test azure automatic deploy (from console
    portal.azure.com) from github

---
+++ Model/ItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingSample.Model
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
