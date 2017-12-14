66669b72686d629cc86dd80d9494e1f938d5e5a5
Roman <roman@Romans-MacBook-Pro.local>
2017-12-14 21:17:57 +0200

    return column

--- LoggingSample.Migrations/Sql/20171214191713_Return_Delayed.Designer.cs
+++ LoggingSample.Migrations/Sql/20171214191713_Return_Delayed.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LoggingSample.Migrations.Sql
 {
     [DbContext(typeof(TodoContext))]
-    partial class TodoContextModelSnapshot : ModelSnapshot
+    [Migration("20171214191713_Return_Delayed")]
+    partial class Return_Delayed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
